eb5f35a8a2a90a658af2a3fc0ff5b7ebd05d26af
chankruze <chankruze@gmail.com>
2020-11-24 10:52:40 +0530

    Add webpack config
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Nov 24 2020 07:00:18 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      // Files: js/jsx,
+      // Pipes: babel-loader
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      // Files: sass/scss,
+      // Pipes: css-loader
+      {
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      // Files: html,
+      // Pipes: html-loader
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+      hash: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".scss", ".sass"],
+  },
+};
